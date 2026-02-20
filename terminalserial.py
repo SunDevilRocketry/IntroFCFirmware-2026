cbee52483c550d9c6e24275f6deab4222660c319
fallencoffee <aaron.tsang@hotmail.com>
2026-02-20 02:17:15 -0700

    Update terminalserial.py
    might need a loop to return/ write multiple bytes of data. Unknown if commands are valid.

--- terminalserial.py
+++ terminalserial.py
@@ -1,18 +1,29 @@
 #terminalserial.py
+import serial
 
 class SerialTerminal:
-    def __init__(self, comport, baudrate):
+    def __init__(self, comport, baudrate,numbytes):
         self.__comport = comport
         self.__baudrate = baudrate
-        
-
+        serialinfo = open(comport,baudrate)
+        read_test = serial.SerialException
+        self.__numbytes = numbytes
         def available_ports():
+            if read_test == "":
+                print(serial.tools.list_ports.comports(include_links=False))
+            else:
+                print(serial.SerialException)
             #direct pyserial call avail COM ports, gather all available in a list and returns
-#create an if statement to check for connection issues before function call
+            #create an if statement to check for connection issues before function call
             pass
         def read(read_byte):
-            #pyserial call
+            #pyserial read
+            timeout_exception = serial.SerialTimeoutException
+            return serialinfo.read(read_byte)
+            if serial.SerialTimeoutException == "b''":
+                read()
             pass
         def write(write_byte):
-            #pyserial call
+            #pyserial write
+            serialinfo.write(write_byte)
             pass
