f7af9721b1eb46657dca099219f8cb67ca78ed68
bigmouseears <nathanreynolds762@gmail.com>
2026-02-27 12:53:06 -0700

    Added basic stuff to BMP390.h

--- mod/BMP390.c
+++ mod/BMP390.c
@@ -4,7 +4,7 @@
 #include <stdint.h>
 #include "stm32h7xx_hal_i2c.h"
 
-uint16_t BMP390Addy = 0x76 << 8;
+
 
 int baro_reg_write (uint16_t memaddy, uint8_t* writestuff){
     HAL_StatusTypeDef writeStatus = HAL_I2C_Mem_Write(*hi2c, memaddy, 8, writestuff, sizeof(writestuff), 10);
@@ -16,8 +16,8 @@ int baro_reg_write (uint16_t memaddy, uint8_t* writestuff){
     }
 }
 
-BARO_STATUS baro_reg_read (uint16_t devAddress, uint16_t memAddress, uint8_t *pData, uint8_t size) {
-    HAL_StatusTypeDef readStatus = HAL_I2C_Mem_Read(*hi2c, devaddy, memaddy, I2C_MEMADD_SIZE_8BIT, pData, size);
+BARO_STATUS baro_reg_read (uint16_t memAddress, uint8_t *pData, uint8_t size) {
+    HAL_StatusTypeDef readStatus = HAL_I2C_Mem_Read(&hi2c, BMP390Addy, memAddress, I2C_MEMADD_SIZE_8BIT, pData, size);
     if (readStatus != HAL_OK) {
         return BARO_FAIL;
     }
--- mod/BMP390.h
+++ mod/BMP390.h
@@ -1,7 +1,13 @@
 //hello
 
+// Device Address
+uint16_t BMP390Addy = 0x76 << 1;
+
 // Status Enum
 typedef enum {
     BARO_OK,
     BARO_FAIL
 } BARO_STATUS;
+
+// Function Prototypes
+BARO_STATUS baro_red_read (uint16_t devAddress, uint16_t memAddress, uint8_t *pData, uint8_t size);
